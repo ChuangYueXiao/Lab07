1cf1fb443283cc0323877eb3403305d2c839b041
ChuangYueXiao <116137081+ChuangYueXiao@users.noreply.github.com>
2024-06-04 22:57:57 +0800

    Add files via upload

---
+++ C07_01_2.sql
@@ -0,0 +1,10 @@
+-- MySQL
+START TRANSACTION;
+
+INSERT INTO Shohin2 VALUES ('0001', 'T侐', '衣物', 1000, 500, '2009-09-20');
+INSERT INTO Shohin2 VALUES ('0002', '打孔機', '辦公用品', 500, 320, '2009-09-11');
+INSERT INTO Shohin2 VALUES ('0003', '襯衫', '衣物', 4000, 2800, NULL);
+INSERT INTO Shohin2 VALUES ('0009', '手套', '衣物', 800, 500, NULL);
+INSERT INTO Shohin2 VALUES ('0010', '水壺', '廚房用品', 2000, 1700, '2009-09-20');
+
+COMMIT;
